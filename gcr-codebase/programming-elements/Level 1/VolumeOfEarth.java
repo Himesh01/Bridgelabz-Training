dab2745e2001906f27ade51361373a64bb54b147
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-17 14:37:18 +0530

    [Himesh] : added Volume Of Earth

---
+++ gcr-codebase/programming-elements/Level 1/VolumeOfEarth.java
@@ -0,0 +1,12 @@
+public class VolumeOfEarth{
+	public static void main(String[]args){
+	int radiusInKm =6378;
+	double radiusInMiles =6378*1.6;
+	final double pi= 3.14;
+	double volumeInKm = (4.0/3.0) * pi * radiusInKm * radiusInKm * radiusInKm;
+	double volumeInMiles = (4.0/3.0) * pi * radiusInMiles * radiusInMiles * radiusInMiles;
+	System.out.println("The volume of earth in cubic kilometers is "+volumeInKm+" and cubic miles is " + volumeInMiles );
+	}
+}
+	
+	

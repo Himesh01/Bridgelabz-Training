cd64c51c6a80c1f2b6b743d93d7fb9f1d2a63e5a
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-17 12:26:00 +0530

    [Himesh] : added Sam's Marks

---
+++ gcr-codebase/programming-elements/Level 1/SamMarks.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class SamMarks{
+	public static void main(String[] args){
+		int maths = 94;
+		int physics = 95;
+		int chemistry =96;
+		int average = (maths + physics + chemistry) / 3;
+		System.out.println("Sam’s average mark in PCM is" + average);
+	}
+}
+		

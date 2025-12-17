647a433d4e079f853bfa1b8c03d09e986bf5a55a
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-17 13:31:04 +0530

    [Himesh] : added Equal division

---
+++ gcr-codebase/programming-elements/Level 1/Eqidivision.java
@@ -0,0 +1,10 @@
+public class Eqidivision{
+	public static void main(String[]args){
+	int pens =14;
+	int students =3;
+	int distributedPens =pens/students;
+	int remainingPens = pens % 3;
+	System.out.println("The Pen Per Student is " + distributedPens + " and the remaining pen not distributed is " + remainingPens);
+	}
+}
+	

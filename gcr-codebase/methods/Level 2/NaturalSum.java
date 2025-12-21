a7b69bf7c84428a47ef90032607ad468568c3d54
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-21 10:49:59 +0530

    [Himesh] : added Natural sum logic

--- gcr-codebase/methods/Level 2/NaturalSum.java
+++ gcr-codebase/methods/Level 2/NaturalSum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 import java.util.*;
-public class NaturalSum{
+public class NaturalSum{ 
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();

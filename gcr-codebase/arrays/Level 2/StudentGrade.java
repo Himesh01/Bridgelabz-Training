0cc532104feba1ee36ac2b4ba397456a983ffb8c
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-21 10:45:38 +0530

    [Himesh] : modified files

--- gcr-codebase/arrays/Level 2/StudentGrade.java
+++ gcr-codebase/arrays/Level 2/StudentGrade.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.Arrays;
-
 public class StudentGrade{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
--- gcr-codebase/methods/Level 1/NaturalSum.java
+++ gcr-codebase/methods/Level 1/NaturalSum.java
@@ -1,19 +1,21 @@
 import java.util.*;
 
-public class NaturalSum{
+import java.util.*;
+public class NaturalNumber{
 	public static void main(String[] args){
-	
-	 Scanner sc = new Scanner(System.in);
-	 System.out.println("Enter a number");
-	 int n = sc.nextInt();
-	 int ans  = sumNatural(n);
-	 System.out.println("the sum of n natural numbers " +ans);
-	 }
-	 public static int sumNatural(int n){
-	 int sum =0;
-		for(int i =0; i<=n; i++)
-			sum = sum+i;
-		return sum;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println("The sum of n natural number by formula is "+formula(n));
+		System.out.println("The sum of n natural number by recursion is "+recursion(n));
+	}
+	public static int formula(int n){
+		return (n*(n+1))/2;
+	}
+	public static int recursion(int n){
+		if(n<=0){
+			return 0;
+		}
+		return recursion(n-1)+n;
 	}
 }
-	 

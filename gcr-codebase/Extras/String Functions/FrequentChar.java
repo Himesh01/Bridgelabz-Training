1bdbf3bcaa42a600d73af473a6d8ca1383abdfa9
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-23 22:50:22 +0530

    [Himesh] : added Frequent character file

---
+++ gcr-codebase/Extras/String Functions/FrequentChar.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class FrequentChar{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a String");
+		String s = sc.next();
+		
+		System.out.println("The Most Frequent Character is "+frequent(s));
+	}
+	
+	public static char frequent(String s){
+		int [] arr = new int[256];
+		
+		
+		for(char a : s.toCharArray()){
+		arr[a]+=1;
+		}
+		int idx =0;
+		int max =0;
+		for(int i =0;i<256;i++){
+			if(max<arr[i]){
+				max = arr[i];
+				idx =i;
+			}
+		}
+		return (char)(idx);
+	}
+}

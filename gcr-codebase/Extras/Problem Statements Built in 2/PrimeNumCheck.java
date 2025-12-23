552624a2b1ce67c740b0e20687fc31021eb91f41
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-24 00:09:46 +0530

    [Himesh] : added Prime Number Checker file

---
+++ gcr-codebase/Extras/Problem Statements Built in 2/PrimeNumCheck.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PrimeNumCheck{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a number : ");
+		int n = sc.nextInt();
+		
+		System.out.println("The number is Prime : "+isPrime(n));
+	}
+	public static boolean isPrime(int n){
+		if(n<2){
+			return false;
+		}
+		for(int i =2;i<n;i++){
+			if(n%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
+}

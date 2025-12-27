d69d5cd253e0178587c05d89fbb573d0dc2e0cde
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-27 23:00:09 +0530

    [Himesh] : added Employee Records file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructorInstance/levelone/Employee.java
@@ -0,0 +1,18 @@
+package com.constructorInstance.levelone;
+
+public class Employee {
+
+	public int employeeID;
+	protected String department;
+	private double salary;
+	
+	public Employee(int employeeID, String dept, double salary) {
+		this.employeeID= employeeID;
+		this.department= dept;
+		this.salary = salary;
+	}
+	
+	public void updateSalary(double salary) {
+		this.salary =salary;
+	}
+}

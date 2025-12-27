50d2404cd4c939dd9c5b71b11c33a505dd8fb460
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-27 23:08:01 +0530

    [Himesh] : added Bank Account main file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructorInstance/levelone/SavingsAccount.java
@@ -0,0 +1,17 @@
+package com.constructorInstance.levelone;
+
+public class SavingsAccount extends BankAccount {
+
+	public SavingsAccount(int accNo, String accountHolder, double balance) {
+		super(accNo,accountHolder, balance);
+	}
+	public static void main(String[] args) {
+		SavingsAccount acc = new SavingsAccount(12142, "Himesh", 180000);
+		System.out.println("Account Holder: "+acc.accountHolder);
+		System.out.println("Account number: "+acc.accountNumber);
+		acc.getBalance();
+		acc.setBalance(1000);
+		acc.getBalance();
+	}
+}
+	

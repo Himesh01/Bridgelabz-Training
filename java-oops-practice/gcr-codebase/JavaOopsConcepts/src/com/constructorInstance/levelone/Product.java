b4f36bb945ffd132c14f358fafa3a218757e8505
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-27 16:09:11 +0530

    [Himesh] added Product Inventry System file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructorInstance/levelone/Product.java
@@ -0,0 +1,31 @@
+package com.constructorInstance.levelone;
+
+public class Product {
+	
+	public String productName;
+	public int price;
+	static int count =0;
+	public Product(String prodName, int price) {
+		this.productName = prodName;
+		this.price = price;
+		this.count+=1;
+	}
+	public void displayProductDetails() {
+		System.out.println("Product Name: "+this.productName);
+		System.out.println("Product price: "+this.price);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Product p1 = new Product("Rocket", 1000);
+		Product p2 = new Product("Pen", 50);
+		p1.displayProductDetails();
+		p2.displayProductDetails();
+		displayTotalProducts();
+
+	}
+	public static void displayTotalProducts() {
+			System.out.println("Total product create: "+ count);			
+	}
+}

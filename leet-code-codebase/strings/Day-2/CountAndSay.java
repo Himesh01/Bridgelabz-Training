9f55efebd983fbf996b1aff4697b78795b61c2eb
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-28 21:59:18 +0530

    [Himesh] : added Count And Say(38) file

---
+++ leet-code-codebase/strings/Day-2/CountAndSay.java
@@ -0,0 +1,24 @@
+public class CountAndSay {
+    public String countAndSay(int n) {
+        String result = "1";
+        for (int i = 1; i < n; i++) {
+            result = say(result);
+        }
+        return result;
+    }
+
+    private String say(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            if (i == s.length() || s.charAt(i) != s.charAt(i - 1)) {
+                sb.append(count);
+                sb.append(s.charAt(i - 1));
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        return sb.toString();
+    }
+}

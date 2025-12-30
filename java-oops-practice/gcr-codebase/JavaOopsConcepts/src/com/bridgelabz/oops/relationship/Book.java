3b2d1b6d5ecab034276427f8bcba1f765923f580
Himesh Kurmi <rajendrakurmi729@gmail.com>
2025-12-30 15:28:57 +0530

    [Himesh] : added Library and Books file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/relationship/Book.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.oops.relationship;
+
+
+public class Book {
+	private String title;
+	private String author;
+	
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+	
+	public void display() {
+		System.out.println("title: "+title+", Author: "+author);
+	}
+}

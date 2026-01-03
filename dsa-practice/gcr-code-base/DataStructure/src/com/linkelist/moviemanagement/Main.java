b413733e1b50b6d2e88d990916f20747ef231822
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-03 10:39:10 +0530

    [Himesh] : added Movie management file

---
+++ dsa-practice/gcr-code-base/DataStructure/src/com/linkelist/moviemanagement/Main.java
@@ -0,0 +1,13 @@
+package com.linkelist.moviemanagement;
+
+public class Main {
+	 public static void main(String[] args) {
+    	 DoubleLinkList list = new DoubleLinkList();
+    	 list.add("Dhurandhar ","Himesh","2003", 4.5f);
+    	 list.add("Dhanak ","Rajeev","2025", 3.5f);
+    	 list.display();
+    	 list.updateRating("Dhanak ", 4.5f);
+    	 list.display();
+    	 
+     }
+}

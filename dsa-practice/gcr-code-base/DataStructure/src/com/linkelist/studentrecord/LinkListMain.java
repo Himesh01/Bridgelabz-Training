76f1a704211eb5e092be1fa44af56aa2830c11d4
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-03 10:25:52 +0530

    [Himesh] : added dsa-practice folder

---
+++ dsa-practice/gcr-code-base/DataStructure/src/com/linkelist/studentrecord/LinkListMain.java
@@ -0,0 +1,14 @@
+package com.linkelist.studentrecord;
+
+public class LinkListMain {
+	public static void main(String[] args) {
+    	LinkList list = new LinkList();
+    	list.addStudent("Himesh",22 ,101,'A');
+    	list.addStudent("Lokesh", 21, 102, 'B');
+    	list.atPosition("Ramesh", 23, 103, 'B', 1);
+    	list.atPosition("Ramnesh", 23, 103, 'B', 1);
+    	
+    	list.updateGrade(102, 'A');
+    	list.displayAll();
+    }
+}

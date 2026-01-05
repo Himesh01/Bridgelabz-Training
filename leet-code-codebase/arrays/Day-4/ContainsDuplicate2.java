3e636f8f0e96f14c30dff5865bbac448c1da6432
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-05 05:32:45 +0530

    [Himesh] : added Contains Duplicate 2 (219) leetcode problem

---
+++ leet-code-codebase/arrays/Day-4/ContainsDuplicate2.java
@@ -0,0 +1,19 @@
+/*
+Given an integer array nums and an integer k,
+ return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+ */
+
+class ContainsDuplicate2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i =0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                if(i-map.get(nums[i])<=k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}

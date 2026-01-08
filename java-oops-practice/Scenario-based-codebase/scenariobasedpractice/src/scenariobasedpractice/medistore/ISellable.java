86e30a9375544b488f7953ae5d2a61acbc6417cc
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-08 15:58:49 +0530

    [Himesh] : added MediStore – Pharmacy Inventory & Sales System

---
+++ java-oops-practice/Scenario-based-codebase/scenariobasedpractice/src/scenariobasedpractice/medistore/ISellable.java
@@ -0,0 +1,9 @@
+package scenariobasedpractice.medistore;
+
+import java.time.LocalDate;
+
+public interface ISellable {
+	public boolean checkExpiry(LocalDate date);
+	
+	public void sell(int quantity);
+}

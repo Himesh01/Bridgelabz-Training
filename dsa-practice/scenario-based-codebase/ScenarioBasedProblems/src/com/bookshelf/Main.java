07119e9a1bd83f7af29e0d40f40da238974e4843
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-10 15:27:51 +0530

    [Himesh] : added BookShelf – Library Organizer (HashMap + Linked List)

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bookshelf/Main.java
@@ -0,0 +1,22 @@
+package com.bookshelf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Library library = new Library();
+
+        Book b1 = new Book("1984", "George Orwell");
+        Book b2 = new Book("Animal Farm", "George Orwell");
+        Book b3 = new Book("Dune", "Frank Herbert");
+
+        library.addBook("Fiction", b1);
+        library.addBook("Fiction", b2);
+        library.addBook("Sci-Fi", b3);
+
+        library.displayCatalog();
+
+        library.borrowBook("Fiction", b1);
+        library.displayCatalog();
+    }
+}

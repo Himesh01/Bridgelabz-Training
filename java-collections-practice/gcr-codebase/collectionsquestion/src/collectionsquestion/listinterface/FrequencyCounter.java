89c868cd0a1407eac92d3057dce42e95954725a0
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-16 22:47:05 +0530

    [Himesh] : added Find Frequency of Elements file

---
+++ java-collections-practice/gcr-codebase/collectionsquestion/src/collectionsquestion/listinterface/FrequencyCounter.java
@@ -0,0 +1,19 @@
+package collectionsquestion.listinterface;
+import java.util.*;
+
+public class FrequencyCounter {
+	public static Map<String, Integer> countFrequency(List<String> list){
+		Map<String,Integer> frequencyMap = new HashMap<>();
+		
+		for(String item :list) {
+			frequencyMap.put(item ,  frequencyMap.getOrDefault(item ,  0)+1);
+		}
+		return frequencyMap;
+	}
+	public static void main(String[] args) {
+		List<String> input  = Arrays.asList("apple","mango","banana","mango");
+		
+		Map<String,Integer> result = countFrequency(input);
+		System.out.println(result);
+	}
+}

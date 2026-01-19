ef44f243fcb50f4f3aa0ff390338d608634e903b
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-19 23:11:02 +0530

    [Himesh] : added  FlashDealz – Product Sorting by Discount (Quick Sort) file

--- dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/FlashDealz/Product.java
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/FlashDealz/Product.java
@@ -2,4 +2,16 @@ package com.FlashDealz;
 
 public class Product {
 
+	public String prodName;
+	public double discount;
+	
+	public Product(String name, double discount) {
+		this.prodName = name;
+		this.discount =discount;
+	}
+	
+	@Override
+	public String toString() {
+		return prodName+" - "+discount+" %";
+	}
 }

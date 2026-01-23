5efc574f482f0840778b7143cd4593f6d5b1be94
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-23 14:31:34 +0530

    [Himesh] : added Gaming App - Real-Time Leaderboard file

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/TreeGraph/GamingApp/Main.java
@@ -0,0 +1,22 @@
+package com.TreeGraph.GamingApp;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+
+    	GamingApp leaderboard = new GamingApp();
+
+        leaderboard.insertOrUpdate(new Player("Nageshwar", 1200));
+        leaderboard.insertOrUpdate(new Player("Lucky", 1500));
+        leaderboard.insertOrUpdate(new Player("Himesh", 900));
+        leaderboard.insertOrUpdate(new Player("Rajeev", 1800));
+        leaderboard.insertOrUpdate(new Player("Raj", 1100));
+
+        leaderboard.displayTopPlayers(10);
+
+        System.out.println("\nRemoving player with 1200 points");
+        leaderboard.remove(1200);
+
+        leaderboard.displayTopPlayers(10);
+    }
+}
---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/TreeGraph/GamingApp/Player.java
@@ -0,0 +1,16 @@
+package com.TreeGraph.GamingApp;
+
+public class Player {
+	public String name;
+	public int point;
+	
+	public Player(String name, int point) {
+		this.name = name;
+		this.point = point;
+	}
+	
+	@Override
+	public String toString() {
+		return "Name: "+name+", points: "+point;
+	}
+}

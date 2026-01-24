aa0ee501b7fe5e3d6248cd4226d8406315fed1a9
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-25 00:25:49 +0530

    [Himesh] : added try-with-resources (Auto-closing Resources)

---
+++ java-collections-practice/gcr-codebase/collectionsquestion/src/exception/TryWithResources.java
@@ -0,0 +1,21 @@
+package exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+            String firstLine = br.readLine();
+            System.out.println(firstLine);
+
+        }
+        catch (IOException e) {
+            System.out.println("Exception Handled: " + e.getClass().getName());
+            System.out.println("Error reading file");
+        }
+    }
+}

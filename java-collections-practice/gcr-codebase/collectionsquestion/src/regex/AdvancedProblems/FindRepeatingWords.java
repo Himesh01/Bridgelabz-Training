c79545b8a03ee5a0272a8f256400f00211f5dc83
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-25 23:29:31 +0530

    [Himesh] : added Find Repeating Words in a Sentence

---
+++ java-collections-practice/gcr-codebase/collectionsquestion/src/regex/AdvancedProblems/FindRepeatingWords.java
@@ -0,0 +1,17 @@
+package regex.AdvancedProblems;
+import java.util.regex.*;
+
+public class FindRepeatingWords {
+	public static void main(String[] args) {
+
+        String text = "This is is a repeated repeated word test.";
+
+        String regex = "\\b(\\w+)\\s+\\1\\b";
+
+        Matcher m = Pattern.compile(regex).matcher(text);
+
+        while(m.find()) {
+            System.out.println(m.group(1));
+        }
+    }
+}

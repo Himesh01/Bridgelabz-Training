ef71b8814bb027f57e248e1d2587b6136f184317
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-25 23:26:36 +0530

    [Himesh] : added Validate a Credit Card Number (Visa, MasterCard, etc.)

---
+++ java-collections-practice/gcr-codebase/collectionsquestion/src/regex/AdvancedProblems/ValidateACreditCardNumber.java
@@ -0,0 +1,15 @@
+package regex.AdvancedProblems;
+
+public class ValidateACreditCardNumber {
+	public static void main(String[] args) {
+
+        String[] cards = { "4123456789012345",  "5123456789012345",    "6123456789012345",    "412345678901234"     };
+
+        String regex = "^(4|5)[0-9]{15}$";
+
+        for(String card : cards) {
+            System.out.println(card + " → " + card.matches(regex));
+        }
+    }
+
+}

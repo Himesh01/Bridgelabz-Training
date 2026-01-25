1cfead029ad14a9b36464df1258016da2f5e08b2
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-26 01:06:44 +0530

    [Himesh] : added Performance Testing Using @Timeout

---
+++ java-collections-practice/gcr-codebase/collectionsquestion/src/jUnit/BasicJUnitProblems/PerformanceTestingUsingTimeout/PerformanceUtils.java
@@ -0,0 +1,8 @@
+package jUnit.BasicJUnitProblems.PerformanceTestingUsingTimeout;
+
+public class PerformanceUtils {
+	public String longRunningTask() throws InterruptedException {
+        Thread.sleep(3000);  
+        return "Done";
+    }
+}

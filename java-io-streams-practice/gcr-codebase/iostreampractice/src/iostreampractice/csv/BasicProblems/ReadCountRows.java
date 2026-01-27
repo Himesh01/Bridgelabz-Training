2440e27adf6bf8b6fe4c1bb9ce0565913780c046
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-01-27 15:46:52 +0530

    [Himesh] : added Read and Count Rows in a CSV File

---
+++ java-io-streams-practice/gcr-codebase/iostreampractice/src/iostreampractice/csv/BasicProblems/ReadCountRows.java
@@ -0,0 +1,29 @@
+package iostreampractice.csv.BasicProblems;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCountRows {
+	public static void main(String[] args) {
+		String filePath = "employee.csv";
+		int count =0;
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+			String line;
+			boolean isHeader = true;
+			while ((line = br.readLine()) != null) {
+                if (isHeader) {
+                    isHeader = false;
+                    continue;
+                }
+                count++;
+            }
+
+            System.out.println("Number of records: " + count);
+
+        } catch (IOException e) {
+            System.out.println("Error reading CSV file");
+            System.out.println("Exception: " + e.getClass().getName());
+        }	
+	}
+}

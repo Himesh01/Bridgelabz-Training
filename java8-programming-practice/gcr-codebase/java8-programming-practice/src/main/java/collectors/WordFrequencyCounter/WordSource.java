11adc2378e6f6ab2ee516188539dad3610de19db
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-02-01 01:50:31 +0530

    [Himesh] : added Word Frequency Counter file

---
+++ java8-programming-practice/gcr-codebase/java8-programming-practice/src/main/java/collectors/WordFrequencyCounter/WordSource.java
@@ -0,0 +1,11 @@
+package collectors.WordFrequencyCounter;
+
+public class WordSource {
+    private String text;
+
+    public WordSource(String text) {
+        this.text = text;
+    }
+
+    public String getText() { return text; }
+}

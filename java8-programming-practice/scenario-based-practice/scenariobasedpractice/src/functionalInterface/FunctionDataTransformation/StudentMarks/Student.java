ddb3cb040060eb995466854625e03a738fad6a70
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-02-03 01:52:35 +0530

    [Himesh] : added  Convert student marks file

---
+++ java8-programming-practice/scenario-based-practice/scenariobasedpractice/src/functionalInterface/FunctionDataTransformation/StudentMarks/Student.java
@@ -0,0 +1,11 @@
+package functionalInterface.FunctionDataTransformation.StudentMarks;
+
+public class Student {
+	String name;
+    int marks;
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}

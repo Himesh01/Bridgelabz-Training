a2ded470427261efc0a2d061469fa170c432e408
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-02-03 01:53:18 +0530

    [Himesh] : added payroll system file

---
+++ java8-programming-practice/scenario-based-practice/scenariobasedpractice/src/functionalInterface/FunctionDataTransformation/payrollsystem/Employee.java
@@ -0,0 +1,11 @@
+package functionalInterface.FunctionDataTransformation.payrollsystem;
+
+public class Employee {
+	String name;
+    double monthlySalary;
+
+    Employee(String name, double monthlySalary) {
+        this.name = name;
+        this.monthlySalary = monthlySalary;
+    }
+}
